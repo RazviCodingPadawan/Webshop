fe8369ab0360be9ba126be28492986474e25e1af
Eijey11 <arturs.jarovojs2018@gmail.com>
2022-05-24 11:59:01 +0200

    footer changes

--- src/StartPage.jsx
+++ src/StartPage.jsx
@@ -5,7 +5,7 @@ export default function StartPage() {
   return <div className="main_container">
     <h1 className="main_text">Phonehub!<br></br><span>”Connect with the world”</span></h1>
     <footer>
-        <a className="footer_link" href="http://red.moon.se">@ RED-moon.se</a>
+        {/* <a className="footer_link" href="http://red.moon.se">@ RED-moon.se</a> */}
     </footer>
   </div>
     
