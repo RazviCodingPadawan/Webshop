481b78520a369043d1dd7048ada8058468e95ab0
Erik <Erikmansoder@outlook.com>
2022-05-24 11:54:55 +0200

    Added Footer

---
+++ src/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './index.css';
+import Logo from "/Volumes/kod/EARN/src/images/logofa.png";
+
+
+const Footer = () => (
+  <div className="footer">
+    
+    <div><img style={{height: 35, width: 35}}src={Logo}/></div>
+  </div>
+);
+
+export default Footer;
