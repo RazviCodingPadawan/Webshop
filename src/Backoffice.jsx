afe58e59c1f176e49e9a725ddacaa7e764b82f65
Razvan <razvan.georgescu89@yahoo.com>
2022-05-25 15:00:03 +0200

    shit icon

--- src/Backoffice.jsx
+++ src/Backoffice.jsx
@@ -13,7 +13,7 @@ export default function Backoffice() {
 
   return <Container className="productList">
     <h1>Welcome to backoffice</h1>
-    <h3>The place where shit happens</h3>
+    <h3>The place where shit happens 💩</h3>
     <Link to="/backoffice/edit">Edit</Link>
     <Link to="/backoffice/create">Create</Link>
     <Link to="/backoffice/delete">Delete</Link>
