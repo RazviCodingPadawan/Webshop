293934ba63cc7bfb0706212ec899c54e04febd6d
”nataliaermeryd <natalia.ermeryd@outlook.com>
2022-05-27 00:09:47 +0200

    nav update effect

--- src/MainNav.jsx
+++ src/MainNav.jsx
@@ -1,6 +1,7 @@
 import { Container, Navbar, Nav } from 'react-bootstrap';
 import { Link } from "react-router-dom";
 import './MainNav.css'
+import './MainNav.scss'
 import Logo from "./images/LOGO.png";
 
 import {useStates} from './utilities/states';
@@ -34,8 +35,10 @@ export default function MainNav() {
           <Link className="nav-link products" to="/product-list">PRODUKTER</Link>
           <Link className="nav-link products" to="/backoffice">BACKOFFICE</Link>
         </Nav>
+
         <Link className="nav-link" to="/shopping-cart"><button className="btn_cart">🛒 {totalNumberOfProductsInCart}</button></Link>
-        <Link className="nav-link" to="/login"><button className="btn_login"> Login</button></Link>
+        <Link className="nav-link" to="/login"><button className="btn_login">LOGIN</button></Link>
+
       </Navbar.Collapse>
     </Container>
   </Navbar>
