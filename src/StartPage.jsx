9847076e91236fa2941a20dfc1046c7155a9b172
Eijey11 <arturs.jarovojs2018@gmail.com>
2022-05-26 23:55:49 +0200

    Added responsive Main Page with Burger-menu

--- src/StartPage.jsx
+++ src/StartPage.jsx
@@ -4,9 +4,11 @@ import Footer from './Footer/Footer'
 
 export default function StartPage() {
   return <div className="main_container">
-            <h1 class="animate__animated animate__wobble animate__slower	3s main_text">Phonehub!<br></br><span>”Connect with the world”</span></h1>
+            <div class="animate__animated animate__wobble animate__slower	3s main_text">
+              <h1 className='main_name'>Phonehub!</h1><br></br>
+              <h3 className="span">”Connect with the world”</h3></div>
         <div>
           <Footer />
         </div>
-  </div>
+      </div>
 }
