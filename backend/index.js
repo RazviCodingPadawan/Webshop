6033bde4f94f2d9e1228b4640033b2fc5b4f6f4f
”nataliaermeryd <natalia.ermeryd@outlook.com>
2022-06-04 00:01:50 +0200

    Fix

--- backend/index.js
+++ backend/index.js
@@ -7,14 +7,18 @@ const imageUpload = require('./image-upload');
 const port = process.env.PORT || 4000;
 const dbPath = '../database/db.sqlite3';
 
+
+
 async function start() {
   const db = await bestSqlite.connect(dbPath);
   const app = express();
-  app.use(express.json());
   app.use(express.static('dist'));
+  app.use(express.json());
   imageUpload(app); // add a route for uploading images
   restApi(app, db);
+
   app.listen(port, () => console.log('Backend listening on http://localhost:' + port));
+  
 }
 
 start();
